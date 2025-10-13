d42ec1c210f3f72d39469bc0c1b1a28fe3e59b66
Sharon <khadijabhuiyan101@gmail.com>
2025-10-13 11:56:24 +0600

    latest update

--- js/script.js
+++ js/script.js
@@ -4,6 +4,10 @@ let searchCloseBtn = document.getElementById('so_close_btn');
 let searchInput = document.getElementById('search_input');
 let searchBtn = document.getElementById('so_search_btn');
 let body = document.body;
+let toggleTrigger = document.getElementById('menu-toggle-btn');
+let mobileNav = document.getElementById('mobile_nav_wrapper'); 
+let header = document.getElementById('header'); 
+let toggleIcon = document.getElementById('toggle-icon');
 
 let searchOpen = () =>{
     searchOverlay.style.display = 'flex';
@@ -22,6 +26,32 @@ document.addEventListener('keydown', (event) => {
         searchClose();
     }
 });
+
+
+
+
+let OpenMobileMenu = () => {
+    mobileNav.style.transform = 'translateX(0)';
+    header.style.transform = 'translateX(-300px)';
+    body.classList.add('mobile_nav_active');
+    toggleIcon.src = "../img/logos/close.webp";
+    toggleIcon.style.animation= "rotateIn .5s ease-in-out";
+}
+let CloseMobileMenu = () => {
+    mobileNav.style.transform = 'translateX(300px)';
+    header.style.transform = 'translateX(0px)';
+    body.classList.remove('mobile_nav_active');
+    toggleIcon.src = "../img/logos/toggle.webp";
+     toggleIcon.style.animation= "rotateOut .5s ease-in-out";
+}
+toggleTrigger.addEventListener('click', () => {
+    if (body.classList.contains('mobile_nav_active')) {
+        CloseMobileMenu();
+    } else {
+        OpenMobileMenu();
+    }
+});
+
 
 
 
